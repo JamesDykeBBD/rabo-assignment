511b859f58337480e59a73fa786c7d3d26ac8046
James Dyke <jamesdyke@Jamess-MacBook-Pro.local>
2025-03-12 16:09:44 +0100

    initial commit

---
+++ src/main/java/nl/rabobank/transactionverifier/TransactionVerifierApplication.java
@@ -0,0 +1,13 @@
+package nl.rabobank.transactionverifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionVerifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionVerifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/rabobank/transactionverifier/TransactionVerifierApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.rabobank.transactionverifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionVerifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
